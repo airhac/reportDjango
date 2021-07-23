b10668eefa6da7f58ddcf9a2aef62e165745f8fc
airhac <oooh32141@gmail.com>
2021-07-23 17:01:46 +0900

    7월 23일 재복습 2 1.  CSS, Style, google font 2.  Model 생성, DB연동

--- reportapp/views.py
+++ reportapp/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
-    return render(request, 'reportapp/hello_world.html')#template이름을 적어준다.
 
+    if request.method == "POST":
+        return render(request, 'reportapp/hello_world.html', context={'text':'POST METHOD!'})#template이름을 적어준다.
+    else:
+        return render(request, 'reportapp/hello_world.html', context={'text': 'GET METHOD!'})
